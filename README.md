39becfcf61ba9b01a5300d4670d4b22fc2c08f28
Jake O'Toole <otoolej33@gmail.com>
2020-07-18 16:58:03 -0500

    started login component. installed realm.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # enter-the-mongodb-realm
 My first attempt at a mongodb realm application.
+
+# Installing Realm for Web
+https://docs.mongodb.com/guides/realm/react-web-quickstart/
---
+++ src/components/UserLogin/UserLogin.js
@@ -0,0 +1,39 @@
+import React, {useState} from "react";
+import { set } from "object-path";
+
+export default function UserLogin(userInfo){
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(username, password);
+  }
+
+  return(
+    <div>
+      <h1>Welcome to the Realm.</h1>
+      <h2>Logged in with user id: {userInfo.id}</h2>
+      {/* get user login information */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          <input 
+            type="text"
+            value={username} 
+            id="username" 
+            placeholder="Username" 
+            onChange={event => setUsername(event.target.value)}/>
+        </label>
+        <label htmlFor="password">
+          <input 
+            type="password"
+            value={password} 
+            id="password" 
+            placeholder="Password" 
+            onChange={event => setPassword(event.target.value)}/>
+        </label>
+        <input type="submit" value="Log in" />
+      </form>
+    </div>
+  )
+}
